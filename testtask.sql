aa824db43d3d343c2d02d9ae3aa952416865498f
Skoder <86963273+Sirzhik@users.noreply.github.com>
2024-06-27 02:59:03 +0300

    Update testtask.sql

--- testtask.sql
+++ testtask.sql
@@ -5,30 +5,7 @@
 -- Dumped from database version 16.3
 -- Dumped by pg_dump version 16.3
 
--- Started on 2024-06-27 00:19:51 EEST
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
---
--- TOC entry 3638 (class 1262 OID 16413)
--- Name: testtask; Type: DATABASE; Schema: -; Owner: postgres
---
-
-CREATE DATABASE testtask WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'C';
-
-
-ALTER DATABASE testtask OWNER TO postgres;
-
-\connect testtask
+-- Started on 2024-06-27 02:10:45 EEST
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -76,7 +53,7 @@ CREATE SEQUENCE public.groups_id_seq
 ALTER SEQUENCE public.groups_id_seq OWNER TO postgres;
 
 --
--- TOC entry 3639 (class 0 OID 0)
+-- TOC entry 3638 (class 0 OID 0)
 -- Dependencies: 216
 -- Name: groups_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -116,7 +93,7 @@ CREATE SEQUENCE public.requests_id_seq
 ALTER SEQUENCE public.requests_id_seq OWNER TO postgres;
 
 --
--- TOC entry 3640 (class 0 OID 0)
+-- TOC entry 3639 (class 0 OID 0)
 -- Dependencies: 221
 -- Name: requests_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -154,7 +131,7 @@ CREATE SEQUENCE public.user_role_id_seq
 ALTER SEQUENCE public.user_role_id_seq OWNER TO postgres;
 
 --
--- TOC entry 3641 (class 0 OID 0)
+-- TOC entry 3640 (class 0 OID 0)
 -- Dependencies: 218
 -- Name: user_role_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -195,7 +172,7 @@ CREATE SEQUENCE public.users_id_seq
 ALTER SEQUENCE public.users_id_seq OWNER TO postgres;
 
 --
--- TOC entry 3642 (class 0 OID 0)
+-- TOC entry 3641 (class 0 OID 0)
 -- Dependencies: 220
 -- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -241,10 +218,10 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 -- Data for Name: groups; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO public.groups VALUES (1, 4, 1) ON CONFLICT DO NOTHING;
-INSERT INTO public.groups VALUES (2, 4, 3) ON CONFLICT DO NOTHING;
-INSERT INTO public.groups VALUES (6, 4, 14) ON CONFLICT DO NOTHING;
-INSERT INTO public.groups VALUES (7, 18, 20) ON CONFLICT DO NOTHING;
+INSERT INTO public.groups (id, manager, subuser) VALUES (1, 4, 1);
+INSERT INTO public.groups (id, manager, subuser) VALUES (2, 4, 3);
+INSERT INTO public.groups (id, manager, subuser) VALUES (6, 4, 14);
+INSERT INTO public.groups (id, manager, subuser) VALUES (7, 18, 20);
 
 
 --
@@ -253,17 +230,18 @@ INSERT INTO public.groups VALUES (7, 18, 20) ON CONFLICT DO NOTHING;
 -- Data for Name: requests; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO public.requests VALUES (1, '{"bottoken": "7421484090:AAFh3mv7YDFEbH7lEcF2tsoYOTLp04tnseI", "chatid": 948212683, "message": "text"}', '{"response": "Sended"}', 1) ON CONFLICT DO NOTHING;
-INSERT INTO public.requests VALUES (2, '{"bottoken": "7421484090:AAFh3mv7YDFEbH7lEcF2tsoYOTLp04tnseI", "chatid": 948212683, "message": "\u0422\u0435\u0441\u0442 \u043a\u0438\u0440\u0438\u043b\u0438\u0446\u0456"}', '{"response": "Sended"}', 1) ON CONFLICT DO NOTHING;
-INSERT INTO public.requests VALUES (3, '{"bottoken": "7421484090:AAFh3mv7YDFEbH7lEcF2tsoYOTLp04tnseI", "chatid": 948212683, "message": "Друга спроба"}', '{"response": "Sended"}', 1) ON CONFLICT DO NOTHING;
-INSERT INTO public.requests VALUES (4, '{"bottoken": "7050916373:AAF6fBgRkw22Ttklg8p_T36JiVXOiuZX45I", "chatid": 6414962502, "message": "тест"}', '{"send_error": "Telegram server says - Bad Request: chat not found"}', 1) ON CONFLICT DO NOTHING;
-INSERT INTO public.requests VALUES (5, '{"bottoken": "7050916373:AAF6fBgRkw22Ttklg8p_T36JiVXOiuZX45I", "chatid": 948212683, "message": "тест"}', '{"response": "Sended"}', 1) ON CONFLICT DO NOTHING;
-INSERT INTO public.requests VALUES (6, '{"bottoken": "7421484090:AAFh3mv7YDFEbH7lEcF2tsoYOTLp04tnseI", "chatid": 948212683, "message": "тест"}', '{"response": "Sended"}', 14) ON CONFLICT DO NOTHING;
-INSERT INTO public.requests VALUES (7, '{"bottoken": "7421484090:AAFh3mv7YDFEbH7lEcF2tsoYOTLp04tnseI", "chatid": 948212683, "message": "тест"}', '{"send_error": "send() got an unexpected keyword argument ''chat_id''"}', 14) ON CONFLICT DO NOTHING;
-INSERT INTO public.requests VALUES (8, '{"bottoken": "7421484090:AAFh3mv7YDFEbH7lEcF2tsoYOTLp04tnseI", "chatid": 948212683, "message": "тест"}', '{"send_error": "send() got an unexpected keyword argument ''chat_id''"}', 14) ON CONFLICT DO NOTHING;
-INSERT INTO public.requests VALUES (9, '{"bottoken": "7421484090:AAFh3mv7YDFEbH7lEcF2tsoYOTLp04tnseI", "chatid": 948212683, "message": "тест"}', '{"response": "Sended"}', 14) ON CONFLICT DO NOTHING;
-INSERT INTO public.requests VALUES (10, '{"bottoken": "7421484090:AAFh3mv7YDFEbH7lEcF2tsoYOTLp04tnseI", "chatid": 948212683, "message": "тест"}', '{"response": "Sended"}', 14) ON CONFLICT DO NOTHING;
-INSERT INTO public.requests VALUES (11, '{"bottoken": "7421484090:AAFh3mv7YDFEbH7lEcF2tsoYOTLp04tnseI", "chatid": 948212683, "message": "тест"}', '{"response": "Sended"}', 14) ON CONFLICT DO NOTHING;
+INSERT INTO public.requests (id, request, response, sender) VALUES (1, '{"bottoken": "7421484090:AAFh3mv7YDFEbH7lEcF2tsoYOTLp04tnseI", "chatid": 948212683, "message": "text"}', '{"response": "Sended"}', 1);
+INSERT INTO public.requests (id, request, response, sender) VALUES (2, '{"bottoken": "7421484090:AAFh3mv7YDFEbH7lEcF2tsoYOTLp04tnseI", "chatid": 948212683, "message": "\u0422\u0435\u0441\u0442 \u043a\u0438\u0440\u0438\u043b\u0438\u0446\u0456"}', '{"response": "Sended"}', 1);
+INSERT INTO public.requests (id, request, response, sender) VALUES (3, '{"bottoken": "7421484090:AAFh3mv7YDFEbH7lEcF2tsoYOTLp04tnseI", "chatid": 948212683, "message": "Друга спроба"}', '{"response": "Sended"}', 1);
+INSERT INTO public.requests (id, request, response, sender) VALUES (4, '{"bottoken": "7050916373:AAF6fBgRkw22Ttklg8p_T36JiVXOiuZX45I", "chatid": 6414962502, "message": "тест"}', '{"send_error": "Telegram server says - Bad Request: chat not found"}', 1);
+INSERT INTO public.requests (id, request, response, sender) VALUES (5, '{"bottoken": "7050916373:AAF6fBgRkw22Ttklg8p_T36JiVXOiuZX45I", "chatid": 948212683, "message": "тест"}', '{"response": "Sended"}', 1);
+INSERT INTO public.requests (id, request, response, sender) VALUES (6, '{"bottoken": "7421484090:AAFh3mv7YDFEbH7lEcF2tsoYOTLp04tnseI", "chatid": 948212683, "message": "тест"}', '{"response": "Sended"}', 14);
+INSERT INTO public.requests (id, request, response, sender) VALUES (7, '{"bottoken": "7421484090:AAFh3mv7YDFEbH7lEcF2tsoYOTLp04tnseI", "chatid": 948212683, "message": "тест"}', '{"send_error": "send() got an unexpected keyword argument ''chat_id''"}', 14);
+INSERT INTO public.requests (id, request, response, sender) VALUES (8, '{"bottoken": "7421484090:AAFh3mv7YDFEbH7lEcF2tsoYOTLp04tnseI", "chatid": 948212683, "message": "тест"}', '{"send_error": "send() got an unexpected keyword argument ''chat_id''"}', 14);
+INSERT INTO public.requests (id, request, response, sender) VALUES (9, '{"bottoken": "7421484090:AAFh3mv7YDFEbH7lEcF2tsoYOTLp04tnseI", "chatid": 948212683, "message": "тест"}', '{"response": "Sended"}', 14);
+INSERT INTO public.requests (id, request, response, sender) VALUES (10, '{"bottoken": "7421484090:AAFh3mv7YDFEbH7lEcF2tsoYOTLp04tnseI", "chatid": 948212683, "message": "тест"}', '{"response": "Sended"}', 14);
+INSERT INTO public.requests (id, request, response, sender) VALUES (11, '{"bottoken": "7421484090:AAFh3mv7YDFEbH7lEcF2tsoYOTLp04tnseI", "chatid": 948212683, "message": "тест"}', '{"response": "Sended"}', 14);
+INSERT INTO public.requests (id, request, response, sender) VALUES (12, '{"bottoken": "7421484090:AAFh3mv7YDFEbH7lEcF2tsoYOTLp04tnseI", "chatid": 948212683, "message": "тест"}', '{"response": "Sended"}', 14);
 
 
 --
@@ -272,9 +250,9 @@ INSERT INTO public.requests VALUES (11, '{"bottoken": "7421484090:AAFh3mv7YDFEbH
 -- Data for Name: user_role; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO public.user_role VALUES (1, 'user') ON CONFLICT DO NOTHING;
-INSERT INTO public.user_role VALUES (2, 'manager') ON CONFLICT DO NOTHING;
-INSERT INTO public.user_role VALUES (3, 'admin') ON CONFLICT DO NOTHING;
+INSERT INTO public.user_role (id, role) VALUES (1, 'user');
+INSERT INTO public.user_role (id, role) VALUES (2, 'manager');
+INSERT INTO public.user_role (id, role) VALUES (3, 'admin');
 
 
 --
@@ -283,18 +261,18 @@ INSERT INTO public.user_role VALUES (3, 'admin') ON CONFLICT DO NOTHING;
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO public.users VALUES (1, 'user1', 'f190ce9ac8445d249747cab7be43f7d5', '2584eacc66905904f791554145e203c9', 1) ON CONFLICT DO NOTHING;
-INSERT INTO public.users VALUES (3, 'user2', 'f190ce9ac8445d249747cab7be43f7d4', '2584eacc66905904f791554145e203c0', 1) ON CONFLICT DO NOTHING;
-INSERT INTO public.users VALUES (4, 'manager1', 'A190ce9ac8445d249747cab7be43f7d4', '3484eacc66905904f791554145e203c0', 2) ON CONFLICT DO NOTHING;
-INSERT INTO public.users VALUES (9, 'user3', '4fc88bd8e0cc3a6048530e9e4e887442', 'bb153c74230b4eb4852158938bcdc18e', 1) ON CONFLICT DO NOTHING;
-INSERT INTO public.users VALUES (14, 'user4', '4fc88bd8e0cc3a6048530e9e4e887442', '40a6a5e0cd139aad4474462a2cdc1033', 1) ON CONFLICT DO NOTHING;
-INSERT INTO public.users VALUES (15, 'admin', 'a02005bfad55ba8f265a532e874142d0', 'ca9f259c4ee8f0d81aef1578fe855752', 3) ON CONFLICT DO NOTHING;
-INSERT INTO public.users VALUES (18, 'manager2', '5cda7d20100f0d44e1c33ef4192dde23', '1728a6e053a9053c0714d846864f3d37', 2) ON CONFLICT DO NOTHING;
-INSERT INTO public.users VALUES (20, 'user0', '827ccb0eea8a706c4c34a16891f84e7b', 'c8d5cc9f394bfb6e0d08ccaf6236d1b9', 1) ON CONFLICT DO NOTHING;
+INSERT INTO public.users (id, username, password, token, roleid) VALUES (1, 'user1', 'f190ce9ac8445d249747cab7be43f7d5', '2584eacc66905904f791554145e203c9', 1);
+INSERT INTO public.users (id, username, password, token, roleid) VALUES (3, 'user2', 'f190ce9ac8445d249747cab7be43f7d4', '2584eacc66905904f791554145e203c0', 1);
+INSERT INTO public.users (id, username, password, token, roleid) VALUES (4, 'manager1', 'A190ce9ac8445d249747cab7be43f7d4', '3484eacc66905904f791554145e203c0', 2);
+INSERT INTO public.users (id, username, password, token, roleid) VALUES (9, 'user3', '4fc88bd8e0cc3a6048530e9e4e887442', 'bb153c74230b4eb4852158938bcdc18e', 1);
+INSERT INTO public.users (id, username, password, token, roleid) VALUES (14, 'user4', '4fc88bd8e0cc3a6048530e9e4e887442', '40a6a5e0cd139aad4474462a2cdc1033', 1);
+INSERT INTO public.users (id, username, password, token, roleid) VALUES (15, 'admin', 'a02005bfad55ba8f265a532e874142d0', 'ca9f259c4ee8f0d81aef1578fe855752', 3);
+INSERT INTO public.users (id, username, password, token, roleid) VALUES (18, 'manager2', '5cda7d20100f0d44e1c33ef4192dde23', '1728a6e053a9053c0714d846864f3d37', 2);
+INSERT INTO public.users (id, username, password, token, roleid) VALUES (20, 'user0', '827ccb0eea8a706c4c34a16891f84e7b', 'c8d5cc9f394bfb6e0d08ccaf6236d1b9', 1);
 
 
 --
--- TOC entry 3643 (class 0 OID 0)
+-- TOC entry 3642 (class 0 OID 0)
 -- Dependencies: 216
 -- Name: groups_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
@@ -303,16 +281,16 @@ SELECT pg_catalog.setval('public.groups_id_seq', 7, true);
 
 
 --
--- TOC entry 3644 (class 0 OID 0)
+-- TOC entry 3643 (class 0 OID 0)
 -- Dependencies: 221
 -- Name: requests_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.requests_id_seq', 11, true);
+SELECT pg_catalog.setval('public.requests_id_seq', 12, true);
 
 
 --
--- TOC entry 3645 (class 0 OID 0)
+-- TOC entry 3644 (class 0 OID 0)
 -- Dependencies: 218
 -- Name: user_role_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
@@ -321,7 +299,7 @@ SELECT pg_catalog.setval('public.user_role_id_seq', 3, true);
 
 
 --
--- TOC entry 3646 (class 0 OID 0)
+-- TOC entry 3645 (class 0 OID 0)
 -- Dependencies: 220
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
@@ -446,7 +424,7 @@ ALTER TABLE ONLY public.users
     ADD CONSTRAINT role_id FOREIGN KEY (roleid) REFERENCES public.user_role(id);
 
 
--- Completed on 2024-06-27 00:19:52 EEST
+-- Completed on 2024-06-27 02:10:46 EEST
 
 --
 -- PostgreSQL database dump complete
